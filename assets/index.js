46a11de196348287c7b4c7e40a14b0c9828b9781
maiavelli <maiacamilledavis@gmail.com>
2022-06-07 20:20:56 -0400

    created package requirements and began array of questions

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,27 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [];
+
+inquirer
+.prompt([
+    {
+        type: 'input',
+        message: 'What is the title of your application?',
+        name: 'project title',
+    },
+    {
+        type: 'input',
+        message: 'Please provide a short description of what your application does.',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What is the usage of this application?',
+        name: 'usage',
+    },
+]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +31,6 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
+
